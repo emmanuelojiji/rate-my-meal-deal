d3b6a6e83f71d30a0248e938ff4988bc00baf507
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-04-02 14:20:45 +0100

    Sign Up / Sign in screen implemented

--- src/Pages/Access.js
+++ src/Pages/Access.js
@@ -1,10 +1,35 @@
 import "./Access.scss";
+import { useState } from "react";
 
 const Access = () => {
+  const [isSignInShown, setIsSignInShown] = useState(true);
   return (
-    <>
-      <h1>Access</h1>
-    </>
+    <div className="Access">
+      {isSignInShown ? (
+        <div className="sign-in-container sign-container">
+            <h1 className="access-title">Sign In</h1>
+
+            <div className="input-container">
+          <input type="text" placeholder="Username"></input> 
+          <input type="text" placeholder="Password"></input>
+          </div>
+
+          <span onClick={() => setIsSignInShown(false)}>Sign up instead</span>
+        </div>
+      ) : (
+        <div className="sign-up-container">
+            <h1 className="access-title">Sign Up</h1>
+
+            <div className="input-container">
+          <input type="text" placeholder="Username"></input> 
+          <input type="text" placeholder="Password"></input>
+          <input type="text" placeholder=" Confirm Password"></input>
+          </div>
+
+          <span onClick={() => setIsSignInShown(true)}>Sign in instead</span>
+        </div>
+      )}
+    </div>
   );
 };
 
