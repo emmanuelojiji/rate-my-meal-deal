42a07a1c6490da7260f86c6a50e3692360116c8c
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-04-02 13:14:47 +0100

    nav test

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,8 +5,8 @@ const NavBar = () => {
     return(
         <>
         <div className="nav-bar">
-            <Link to='/'>Home</Link>
-            <Link to='/Feed'>Feed</Link>
+            <Link to='/home'>Home</Link>
+            <Link to='/'>Feed</Link>
             <Link to='/Profile'>Profile</Link>
         </div>
         </>
